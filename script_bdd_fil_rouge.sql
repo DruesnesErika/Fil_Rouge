0552f438c679940e053ed1d3575cc1122e51d644
Druesnes erika <druesnes.erika@gmail.com>
2021-07-05 09:28:28 +0200

    Update

--- script_bdd_fil_rouge.sql
+++ script_bdd_fil_rouge.sql
@@ -4,26 +4,34 @@ USE fil_rouge;
 CREATE TABLE produits(
    pro_id INT(10) NOT NULL AUTO_INCREMENT,
    pro_nom VARCHAR(70),
-   pro_libellé_court VARCHAR(50),
+   pro_libelle_court VARCHAR(50),
    pro_photo VARCHAR(70),
+   pro_ref VARCHAR(50),
+   pro_marque VARCHAR(50),
    pro_sup_ref VARCHAR(50),
    pro_stock INT,
    pro_stock_alert INT,
    pro_prix DECIMAL(7,2),
    pro_date_ajout DATETIME,
    pro_date_modif DATETIME,
-   pro_libéllé_long VARCHAR(100),
+   pro_libelle_long TEXT,
    PRIMARY KEY(pro_id)
 );
 
+-- INSERT INTO produits (pro_nom, pro_libelle_court, pro_prix)
+-- VALUES ('SAS-75 Alto', 'Saxophone', 620),
+--        ('D6120CE BK', 'Guitare électro-acoustique', 75),
+--        ('ES-8 B', 'Piano de scène', 1256)
+
+
 CREATE TABLE fournisseurs(
    fou_id INT(10) NOT NULL AUTO_INCREMENT,
-   fou_société VARCHAR(50),
-   fou_cp INT,
+   fou_societe VARCHAR(50),
+   fou_cp VARCHAR(6),
    fou_mail VARCHAR(50),
    fou_contact_nom VARCHAR(50),
    fou_adresse VARCHAR(255),
-   fou_phone INT,
+   fou_phone VARCHAR(50),
    fou_pays VARCHAR(50),
    fou_role BOOLEAN NOT NULL,
    PRIMARY KEY(fou_id)
@@ -32,9 +40,9 @@ CREATE TABLE fournisseurs(
 CREATE TABLE clients_particuliers(
    cli_pa_id INT(10) NOT NULL AUTO_INCREMENT,
    cli_pa_adresse VARCHAR(255),
-   cli_pa_phone INT,
+   cli_pa_phone VARCHAR(50),
    cli_pa_mail VARCHAR(50),
-   cli_pa_cp INT,
+   cli_pa_cp VARCHAR(6),
    cli_pa_pays VARCHAR(50),
    cli_pa_nom VARCHAR(50),
    cli_pa_prenom VARCHAR(50),
@@ -47,11 +55,11 @@ CREATE TABLE clients_professionels(
    cli_pro_nom VARCHAR(50),
    cli_pro_prenom VARCHAR(50),
    cli_pro_adresse VARCHAR(255),
-   cli_pro_phone INT,
+   cli_pro_phone VARCHAR(50),
    cli_pro_mail VARCHAR(50),
-   cli_pro_cp INT,
+   cli_pro_cp VARCHAR(6),
    cli_pro_coheficient DECIMAL(5,2),
-   cli_pro_société VARCHAR(50),
+   cli_pro_societe VARCHAR(50),
    cli_pro_cli_ref VARCHAR(50),
    PRIMARY KEY(cli_pro_id)
 );
@@ -103,7 +111,7 @@ CREATE TABLE service_commercial(
    FOREIGN KEY(cli_pro_id) REFERENCES clients_professionels(cli_pro_id)
 );
 
-CREATE TABLE OCCUPER(
+CREATE TABLE occuper(
    fou_id INT,
    equ_id INT,
    PRIMARY KEY(fou_id, equ_id),
@@ -111,7 +119,7 @@ CREATE TABLE OCCUPER(
    FOREIGN KEY(equ_id) REFERENCES equipe(equ_id)
 );
 
-CREATE TABLE FOURNIR(
+CREATE TABLE fournir(
    pro_id INT,
    fou_id INT,
    PRIMARY KEY(pro_id, fou_id),
@@ -119,7 +127,7 @@ CREATE TABLE FOURNIR(
    FOREIGN KEY(fou_id) REFERENCES fournisseurs(fou_id)
 );
 
-CREATE TABLE CLASSER(
+CREATE TABLE classer(
    pro_id INT,
    cat_id INT,
    PRIMARY KEY(pro_id, cat_id),
@@ -127,7 +135,7 @@ CREATE TABLE CLASSER(
    FOREIGN KEY(cat_id) REFERENCES categories(cat_id)
 );
 
-CREATE TABLE DEFINIR(
+CREATE TABLE definir(
    cat_id INT,
    equ_id INT,
    PRIMARY KEY(cat_id, equ_id),
@@ -135,7 +143,7 @@ CREATE TABLE DEFINIR(
    FOREIGN KEY(equ_id) REFERENCES equipe(equ_id)
 );
 
-CREATE TABLE MODIFIER(
+CREATE TABLE modifier(
    pro_id INT,
    equ_id INT,
    PRIMARY KEY(pro_id, equ_id),
@@ -143,12 +151,12 @@ CREATE TABLE MODIFIER(
    FOREIGN KEY(equ_id) REFERENCES equipe(equ_id)
 );
 
-CREATE TABLE LISTE_COMMANDES(
+CREATE TABLE liste_commandes(
    pro_id INT,
    com_id INT,
    taxe DECIMAL(5,2),
    reduction INT,
-   prix_unité_ht DECIMAL(5,2),
+   prix_unite_ht DECIMAL(5,2),
    paiement BOOLEAN NOT NULL,
    PRIMARY KEY(pro_id, com_id),
    FOREIGN KEY(pro_id) REFERENCES produits(pro_id),
